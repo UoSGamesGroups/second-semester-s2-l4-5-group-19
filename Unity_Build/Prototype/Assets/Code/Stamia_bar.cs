4bd7283d55d88c737c5ad5f731777113e67abf0a
Ochuko97 <ochuko97@hotmail.com>
2017-01-29 22:10:01 +0000

    Countinued work on protoype
    New additions to the prototype include:
    
    .An effect trail following the bird once launched, allowing the
    trajectory and route traveled to be spotted.
    
    .Thrusters that either project the bird up or forward depending on the
    key pressed. The thrusters are fueled by the amount of stamina of the
    player has.
    
    . A stamina function that deducts stamina whenever thrusters are
    utilized. (function not properly working)

---
+++ Unity_Build/Prototype/Assets/Code/Stamia_bar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamia_bar : MonoBehaviour {
+
+	public Rigidbody2D bird; 
+
+	public float thrust = 20.0f; 
+
+	public float cooldown = 2f; 
+
+	public float Stamina = 100f; 
+
+	public GameObject Staminabar; 
+
+	private bool OutOfStamina = false; 
+
+	// Use this for initialization
+	void Start () {
+		bird = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	//void Update() {
+		//if (cooldown <= Time.time);
+	//}
+
+	void Update () { 
+		//if (cooldown <= Time.time) { 
+		//if (OutOfStamina == true) 
+		//{
+			//Debug.Log("Out of Stamina") 
+		//}
+		
+		 //if (OutOfStamina == false) 
+		//{
+			if (Input.GetKey (KeyCode.Q))
+				bird.AddForce (transform.up * thrust); 
+				//Stamina - 50f;
+			//bird.AddForce(transform.right * thrust); 
+			if (Input.GetKey (KeyCode.E))
+				bird.AddForce (transform.right * thrust);
+				//Stamina - 50f;
+			//if (Stamina == 0f); 
+				//OutOfStamina = true;
+		}
+	}
+//} 
+
