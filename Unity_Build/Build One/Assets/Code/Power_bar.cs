1757a4b5ba600bec43c1e705577c83a23a936a3d
Ochuko97 <ochuko97@hotmail.com>
2017-02-02 01:11:37 +0000

    Created Power bar function to replace sling function

---
+++ Unity_Build/Build One/Assets/Code/Power_bar.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+
+public class Power_bar : MonoBehaviour {
+
+	public Image Powerbar; 
+
+	public float Fill; 
+
+	public float Force; 
+
+	public Rigidbody2D Bird;
+
+	private bool Powermax; 
+
+
+	// Use this for initialization
+	void Start () {
+		if (Fill == 1) {
+			Powermax = true;
+		}
+	}
+
+	private void UpdatePower() {
+		Powerbar.rectTransform.localScale = new Vector3 (Fill, 1, 1);
+	}
+
+	private void AddPower() { 
+		Fill += 0.01f; 
+		UpdatePower ();
+	}
+
+	private void ReducePower() {
+		Fill -= 0.01f; 
+		UpdatePower ();
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Powermax) { 
+			ReducePower();  
+			//Fill -= 0.01f; 
+			//Debug.Log ("power maxed");
+			//UpdatePower(); 
+			if (Fill == 0) {
+				AddPower();
+			}
+		} 
+
+		//else if (Powermax == false) {
+			//Fill += 0.01f; 
+			//UpdatePower(); 
+		//}
+	}
+}
---
+++ Unity_Build/Build One/Assets/Code/Stamia_bar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+public class Stamia_bar : MonoBehaviour {
+
+	public Rigidbody2D bird; 
+
+	public float thrust = 20.0f; 
+
+	public float Stamina = 1f; 
+
+	public Image Staminabar; 
+
+	public Text Staminartxt; 
+	 
+	private bool OutOfStamina = false; 
+
+
+
+	void Start () {
+		
+	}
+
+	private void UpdateStamina() {
+		Staminabar.rectTransform.localScale = new Vector3 (Stamina, 1, 1);
+	}
+
+	void Update () { 
+		
+		if (OutOfStamina == true) 
+		{
+			Staminartxt.text = ("Out of Stamina");
+		}
+
+
+		else if(Input.GetKey (KeyCode.Q)){	
+				bird.AddForce (transform.up * thrust); 
+				Stamina -= 0.004f;  
+			UpdateStamina();
+		}
+
+		else if (Input.GetKey (KeyCode.E)){
+				bird.AddForce (transform.right * thrust);
+				Stamina -= 0.001f; 
+			UpdateStamina ();
+
+		}
+
+		else if (Stamina < 0f){ 
+			 	Stamina = 0f;
+				OutOfStamina = true;
+		}
+	} 
+}
