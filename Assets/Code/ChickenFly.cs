a44f71702572f9ddaf3effc7182a48df8e00b760
Sam0nline <Sam0nline@users.noreply.github.com>
2017-02-07 15:04:36 +0000

    Player 1 & 2 Inputs
    Rough key inputs - they will be modified in tandem with our game.
    Penguin holds Player 1 inputs.
    Chicken holds Player 2 inputs.

---
+++ Assets/Code/ChickenFly.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChickenFly : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public bool IsGrounded = false;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movementx = new Vector3(Input.GetAxis("Vertical"), 0, 0);
+        transform.position += movementx * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Return) && IsGrounded == true)
+        {
+            rb.velocity = new Vector3(0, 5, 0);
+            IsGrounded = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Platform")
+        {
+            IsGrounded = true;
+        }
+    }
+}
