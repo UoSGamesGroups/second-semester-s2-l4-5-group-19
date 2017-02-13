5d78cb4e331a086bfd0ef08b95615c6977a6878a
Sam0nline <gocsamo@gmail.com>
2017-02-13 15:56:43 +0000

    Implemented clouds
    Cloud and level boundaries implemented for both players as well as a
    minor improvement of the camera position at the slingshot.

---
+++ Unity_Build/Build 2.2/Assets/Code/Playerone/Catapult_rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult_rotate : MonoBehaviour {
+
+	public float speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A))
+			transform.Rotate (Vector3.forward * 2);
+		if (Input.GetKey (KeyCode.D))
+			transform.Rotate (Vector3.forward * -2);
+	}
+}
---
+++ Unity_Build/Build 2.2/Assets/Code/Playertwo/Playertwo_catapult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playertwo_catapult : MonoBehaviour {
+
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Rotate (Vector3.forward * 2);
+		if (Input.GetKey (KeyCode.RightArrow))
+			transform.Rotate (Vector3.forward * -2);
+	}
+}
