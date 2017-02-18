bd4bcd5dacf132579951ff31bea654c5d720a1be
Ochuko97 <ochuko97@hotmail.com>
2017-02-18 15:59:54 +0000

    Created new build an completed implementing and coding wind functionality.
    • The game stores seven different wind variables, these variables range
    from: -3, -2, -1, 0, 1, 2, 3.
    • -3, -2 and - 1 attain a negative wind direction meaning the wind
    created from these variables will push the player to the left whilst the
    wind created from 1, 2 and 3 attain positive wind directions and will
    push the birds further to the right.
    • -3 and 3 have the highest of their respected power variables whilst -1
    and 1 have the lowest.
    • 0 attains no wind variable and is the default setting for our game.
    
    • When the game is launched one of a wind variable is randomly
    generated.
    • After 2.5 seconds the force and direction of the wind will begin to
    gradually decrease or increase depending on the variable generated at
    random.
    
    Whilst implementing the functionality I noticed a slight issue. As the
    power and direction of the wind is always fluctuating the player’s bird
    in turn never comes to a standstill (the round never ends). To combat
    this issue I had implemented a functionality in our code that would
    revert the wind power and direction to its default value of zero after
    45 seconds of gameplay, allowing us to avoid a never ending round.
    
    Currently the only visual feedback that suggest to the fluctuation of
    wind are the “Wind direction” arrows found in our UI. Whilst working on
    the task I experimented with implemented particles as visual feedback
    however, during the implementations of the particles I found that the
    birds were traveling faster than the particles could render on screen.

---
+++ Unity_Build/Build 3.5/Assets/Code/Playerone/Power_bar.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Power_bar : MonoBehaviour { 
+
+	public float Fill = 0; 
+
+	public Image bar; 
+
+	public float Multiplier; 
+
+	public float Force;  
+
+	public GameObject Camera1; 
+
+	public GameObject Camera2; 
+
+	public GameObject Camera3; 
+
+	public GameObject Camera4;
+
+	public Rigidbody2D Bird; 
+
+	public Rigidbody2D Peng; 
+
+	public float Player1Speed; 
+
+	public float Player2Speed;
+
+	private bool Powermax = false; 
+
+	private bool P1launched = false; 
+
+	private bool P2launched = false;
+
+	//private bool Powerempty = false;
+
+
+	// Use this for initialization
+	void Start () { 
+		Bird.isKinematic = true;
+		Peng.isKinematic = true;
+		Player1Speed = Bird.velocity.magnitude; 
+		Player2Speed = Peng.velocity.magnitude;
+		Camera2.SetActive (false);
+		Camera4.SetActive (false);
+		if (Fill == 0) {
+			Powermax = false;		
+		}
+
+		if (Fill == 1) {
+			Powermax = true;
+		}
+	}
+
+
+	private void UpdatePower() {
+		bar.rectTransform.localScale = new Vector3 (Fill, 1, 1);
+	} 
+
+	private void ApplyPower() {
+		Force = Fill * Multiplier;
+	}  
+
+
+
+	// Update is called once per frame
+	void Update () { 
+		if(Player1Speed > 1.2 && P1launched == false)
+		{
+			P1launched = true;
+		}
+
+		if(Player2Speed > 1.2 && P2launched == false)
+		{
+			P2launched = true;
+		}
+
+		if (Powermax == false) {
+			Fill += 0.01f; 
+			UpdatePower ();
+		}
+			
+			if (Fill == 1) {
+				Powermax = true;
+			}
+
+			else if (Fill == 1 || Fill>=1 ) {
+				Powermax = true;
+				Fill -= 0.01f;  
+			}
+		if (Powermax == true) {
+			Fill -= 0.01f; 
+			UpdatePower ();
+		}
+
+		if (Fill == 0) {
+			Powermax = false;
+		}
+
+		else if (Fill == 0 || Fill<=0 ) {
+			Powermax = false;
+			Fill += 0.01f;  
+		} 
+		if (Input.GetKey (KeyCode.Space)) { 
+			Bird.isKinematic = false; 
+			ApplyPower (); 
+			Bird.AddForce (transform.right * Force); 
+			Bird.AddForce (transform.up * Force); 
+			Camera1.SetActive(false);
+			Camera2.SetActive (true); 
+			bar.rectTransform.localScale = new Vector3 (0, 1, 1); 
+}  
+		if (P1launched == true && Player1Speed < 0.1)
+		{
+			Camera2.SetActive (false);
+			Camera1.SetActive(true);
+		}
+
+		if (Input.GetKey (KeyCode.Return)) { 
+			Peng.isKinematic = false; 
+			ApplyPower (); 
+			Peng.AddForce (transform.right * Force); 
+			Peng.AddForce (transform.up * Force); 
+			Camera3.SetActive(false);
+			Camera4.SetActive (true); 
+			bar.rectTransform.localScale = new Vector3 (0, 1, 1); 
+		}  
+		if (P2launched == true && Player2Speed < 0.1)
+		{
+			Camera4.SetActive (false);
+			Camera3.SetActive(true);
+		}
+}
+}
---
+++ Unity_Build/Build 3.5/Assets/Code/Playertwo/Playertwo_Stamina.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+public class Playertwo_Stamina : MonoBehaviour {
+
+	public Rigidbody2D Peng; 
+
+	public float thrust = 20.0f; 
+
+	public float Stamina = 1f; 
+
+	public Image Staminabar; 
+
+	public Text Staminartxt; 
+
+	private bool OutOfStamina = false; 
+
+
+
+	void Start () {
+		if (Stamina == 0f) {
+			OutOfStamina = true;
+		}
+	}
+
+	private void UpdateStamina() {
+		Staminabar.rectTransform.localScale = new Vector3 (Stamina, 1, 1);
+	}
+
+	void Update () { 
+
+		if (OutOfStamina == true) 
+		{
+			Staminartxt.text = ("Out of Stamina");
+		}
+
+
+		else if(Input.GetKey (KeyCode.I)){	
+			Peng.AddForce (transform.up * thrust); 
+			Stamina -= 0.004f;  
+			UpdateStamina();
+		}
+
+		else if (Input.GetKey (KeyCode.P)){
+			Peng.AddForce (transform.right * thrust);
+			Stamina -= 0.001f; 
+			UpdateStamina ();
+
+		}
+
+		else if (Stamina == 0f || Stamina <= 0f){ 
+			//Stamina < 0f;
+			OutOfStamina = true;
+		}
+	} 
+}
+
