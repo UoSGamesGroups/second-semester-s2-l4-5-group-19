e0cf1df7ddf4854c6957976ca8cbb50179d4c92d
Sam0nline <gocsamo@gmail.com>
2017-02-18 14:07:26 +0000

    Demo Scene implemented for Parallax

---
+++ Unity_Build/Build Three/Assets/Scene/Demo Assets/MoveCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public float speed = 5f;
+
+	public void Update ()
+    {
+        var movementx = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += movementx * speed * Time.deltaTime;
+    }
+}
