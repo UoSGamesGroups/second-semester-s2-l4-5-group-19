9a613baabbaacd37857603f0bd3ab48a6d6990c6
Ochuko97 <ochuko97@hotmail.com>
2017-02-20 11:10:27 +0000

    Commited new catapult scripts.

--- Unity_Build/Build 3.5/Assets/Code/Playerone/Catapult_rotate.cs
+++ Unity_Build/Build 3.5/Assets/Code/Playerone/Catapult_rotate.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Catapult_rotate : MonoBehaviour {
 
+
 	public float speed = 10.0f;
+	public BirdDistance p1Distance;
 	// Use this for initialization
 	void Start () {
+
 
 	}
 
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.A))
-			transform.Rotate (Vector3.forward * 2);
-		if (Input.GetKey (KeyCode.D))
-			transform.Rotate (Vector3.forward * -2);
+	void Update()
+	{
+
+		bool getLaunch = p1Distance.GetComponent<BirdDistance>().launched;
+		if (Input.GetKey(KeyCode.A) && getLaunch == false)
+		{
+			transform.Rotate(Vector3.forward * 2);
+		}
+
+		if (Input.GetKey(KeyCode.D) && getLaunch == false)
+		{
+			transform.Rotate(Vector3.forward * -2);
+		}
 	}
 }
--- Unity_Build/Build 3.5/Assets/Code/Playertwo/Playertwo_catapult.cs
+++ Unity_Build/Build 3.5/Assets/Code/Playertwo/Playertwo_catapult.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class Playertwo_catapult : MonoBehaviour {
 
+
+
 	public float speed = 5.0f;
+	public BirdDistance p2Distance;
 	// Use this for initialization
 	void Start () {
-
+		;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow))
-			transform.Rotate (Vector3.forward * 2);
-		if (Input.GetKey (KeyCode.RightArrow))
-			transform.Rotate (Vector3.forward * -2);
+	void Update()
+	{
+		bool getLaunch = p2Distance.GetComponent<BirdDistance>().launched;
+		if (Input.GetKey(KeyCode.LeftArrow) && getLaunch == false)
+		{
+			transform.Rotate(Vector3.forward * 2);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow) && getLaunch == false)
+		{
+			transform.Rotate(Vector3.forward * -2);
+		}
 	}
 }
