e4ab39a7590cf6c69a8b60870dea6ca5ffa9feaf
Ochuko97 <ochuko97@hotmail.com>
2017-03-14 23:27:37 +0000

    Created new stamina functionality.

---
+++ Unity_Build/Current build/Assets/Code/GodStrike Stamia.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+public class GodStrikeStamia : MonoBehaviour {
+
+	public Rigidbody2D Playerone; 
+
+	public Rigidbody2D Playertwo; 
+
+	public float Thrust; 
+
+	public float Thunderstrikes = 3f; 
+
+	private bool CanThunderStrike = false; 
+
+	// Use this for initialization
+	void Start () {
+		if (Thunderstrikes == 3f) {
+			CanThunderStrike = true; 
+			Debug.Log ("Can Thunder Strike");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity_Build/Current build/Assets/ThunderDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderDestroy : MonoBehaviour { 
+
+	public GameObject thunder; 
+
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (ThunderDestory ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	} 
+
+	IEnumerator ThunderDestory(){
+		yield return new WaitForSeconds (1); 
+		Destroy(thunder); 
+
+	}
+}
