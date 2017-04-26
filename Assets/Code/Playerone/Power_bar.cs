2270a50adc31fca8da3f0ceaa714d2735f639c9e
ThomasJamesBarrett <s183456@ucs.ac.uk>
2017-04-26 02:55:34 +0100

    Update Power_bar.cs

--- Assets/Code/Playerone/Power_bar.cs
+++ Assets/Code/Playerone/Power_bar.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,6 +11,8 @@ public class Power_bar : MonoBehaviour {
 	public float Multiplier; 
 
 	public float Force;  
+
+	public float LateForce;
 
 	public GameObject Camera1; 
 
@@ -32,7 +34,15 @@ public class Power_bar : MonoBehaviour {
 
 	private bool P1launched = false; 
 
-	private bool P2launched = false;
+	private bool P2launched = false; 
+
+	private bool DelayedTime = false;
+
+    private bool OneLaunch = false;
+    private bool TwoLaunch = false;
+
+    public BirdDistance p1Distance;
+	public BirdDistance p2Distance;
 
 	//private bool Powerempty = false;
 
@@ -60,77 +70,101 @@ public class Power_bar : MonoBehaviour {
 	} 
 
 	private void ApplyPower() {
-		Force = Fill * Multiplier;
-	}  
+		Force = Fill * Multiplier; 
+		LateForce = Force / 2; 
+	}
 
 
 
-	// Update is called once per frame
-	void Update () { 
-		if(Player1Speed > 1.2 && P1launched == false)
-		{
+    // Update is called once per frame
+    void Update() {
+		if (Player1Speed > 1.2 && P1launched == false) {
 			P1launched = true;
 		}
 
-		if(Player2Speed > 1.2 && P2launched == false)
-		{
+		if (Player2Speed > 1.2 && P2launched == false) {
 			P2launched = true;
 		}
 
 		if (Powermax == false) {
-			Fill += 0.01f; 
+			Fill += 0.01f;
 			UpdatePower ();
 		}
-			
-			if (Fill == 1) {
-				Powermax = true;
-			}
 
-			else if (Fill == 1 || Fill>=1 ) {
-				Powermax = true;
-				Fill -= 0.01f;  
-			}
+		if (Fill == 1) {
+			Powermax = true;
+		} else if (Fill == 1 || Fill >= 1) {
+			Powermax = true;
+			Fill -= 0.01f;
+		}
 		if (Powermax == true) {
-			Fill -= 0.01f; 
+			Fill -= 0.01f;
 			UpdatePower ();
 		}
 
 		if (Fill == 0) {
 			Powermax = false;
-		}
-
-		else if (Fill == 0 || Fill<=0 ) {
+		} else if (Fill == 0 || Fill <= 0) {
 			Powermax = false;
-			Fill += 0.01f;  
-		} 
-		if (Input.GetKey (KeyCode.Space)) { 
-			Bird.isKinematic = false; 
-			ApplyPower (); 
-			Bird.AddForce (transform.right * Force); 
-			Bird.AddForce (transform.up * Force); 
-			Camera1.SetActive(false);
-			Camera2.SetActive (true); 
-			bar.rectTransform.localScale = new Vector3 (0, 1, 1); 
-}  
-		if (P1launched == true && Player1Speed < 0.1)
-		{
+			Fill += 0.01f;
+		}
+		//bool getLaunch = p1Distance.GetComponent<BirdDistance>().launched;
+		if (Input.GetKey (KeyCode.Space) && OneLaunch == false) {
+            OneLaunch = true;
+			Bird.isKinematic = false;
+			ApplyPower ();
+			Bird.AddForce (transform.right * Force / 2);
+			Bird.AddForce (transform.up * Force / 10);
+			Camera1.SetActive (false);
+			Camera2.SetActive (true);
+			bar.rectTransform.localScale = new Vector3 (0, 1, 1);
+		}
+		if (P1launched == true && Player1Speed < 0.1) {
 			Camera2.SetActive (false);
-			Camera1.SetActive(true);
+			Camera1.SetActive (true);
 		}
 
-		if (Input.GetKey (KeyCode.Return)) { 
-			Peng.isKinematic = false; 
-			ApplyPower (); 
-			Peng.AddForce (transform.right * Force); 
-			Peng.AddForce (transform.up * Force); 
-			Camera3.SetActive(false);
-			Camera4.SetActive (true); 
-			bar.rectTransform.localScale = new Vector3 (0, 1, 1); 
-		}  
-		if (P2launched == true && Player2Speed < 0.1)
-		{
-			Camera4.SetActive (false);
-			Camera3.SetActive(true);
+		//bool newgetLaunch = p2Distance.GetComponent<BirdDistance>().launched;
+		if (Input.GetKey (KeyCode.Return) && TwoLaunch == false) {
+            TwoLaunch = true;
+			Peng.isKinematic = false;
+			ApplyPower ();
+			Peng.AddForce (transform.right * Force / 2);
+			Peng.AddForce (transform.up * Force / 10);
+			Camera3.SetActive (false);
+			Camera4.SetActive (true);
+			bar.rectTransform.localScale = new Vector3 (0, 1, 1);
 		}
-}
-}
+		if (P2launched == true && Player2Speed < 0.1) {
+			Camera4.SetActive (false);
+			Camera3.SetActive (true);
+		}
+		else if (DelayedTime == false)  {
+			StartCoroutine (Late()); 
+			DelayedTime = true;
+		} 
+	}
+
+
+        IEnumerator Late(){
+            yield return new WaitForSeconds(5);
+                Bird.isKinematic = false;
+                ApplyPower();
+                Bird.AddForce(transform.right * LateForce/ 2);
+                Bird.AddForce(transform.up * LateForce / 10);
+                Camera1.SetActive(false);
+                Camera2.SetActive(true);
+                //bar.rectTransform.localScale = new Vector3(0, 1, 1)
+                Peng.isKinematic = false;
+                ApplyPower();
+                Peng.AddForce(transform.right * LateForce / 2);
+                Peng.AddForce(transform.up * LateForce / 10);
+                Camera3.SetActive(false);
+                Camera4.SetActive(true);
+                //bar.rectTransform.localScale = new Vector3(0, 1, 1); 
+
+            }
+        }
+    
+
+
